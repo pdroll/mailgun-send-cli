a2e0d6617d40cbfa703132e41c35bd5909278280
Pete Droll <droll.p@gmail.com>
2021-01-03 00:10:43 -0500

    Begin adding tests

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  // collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for
+  // which coverage information should be collected
+  collectCoverageFrom: ['lib/*'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+};
