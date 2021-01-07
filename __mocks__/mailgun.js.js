9d0092281a0eeaec59a01ea6359c253782b61ba2
Pete Droll <droll.p@gmail.com>
2021-01-06 22:31:10 -0500

    Continue with MailgunSend#send tests

--- __mocks__/mailgun.js.js
+++ __mocks__/mailgun.js.js
@@ -1,22 +1,28 @@
 const faker = require('faker');
 
-function mailgunClientMock({ apiKey, domain }) {
-  this.apiKey = apiKey;
-  this.domain = domain;
-
-  this.createMessageMock = jest.fn(async (domain, data) => {
-    // if (this.apiKey === 'FAIL_UNAUTHORIZED') {
-    //   throw new Error('')
-    // }
-
-    return {
-      id: faker.random.uuid(),
-      message: 'Queued. Thank you.'
-    }
-  });
+function mailgunClientMock({ key, username }) {
+  this.apiKey = key;
+  this.username = username;
 
   this.messages = {
-    create: this.createMessageMock,
+    create: jest.fn(async () => {
+      if (this.apiKey === 'FAIL_UNAUTHORIZED') {
+        throw new Error({ status: 401 });
+      }
+
+      if (this.apiKey === 'FAIL_NETWORK') {
+        throw new Error({ type: 'EUNAVAILABLE' });
+      }
+
+      if (this.apiKey === 'FAIL_OTHER') {
+        throw new Error('Failed');
+      }
+
+      return {
+        id: faker.random.uuid(),
+        message: 'Queued. Thank you.',
+      };
+    }),
   };
 
   return this;
